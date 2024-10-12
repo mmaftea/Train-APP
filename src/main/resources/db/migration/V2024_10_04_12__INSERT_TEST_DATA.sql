ab4fcaacc20397136ea0bdbe9e2558c7008ef1ee
ssg2655 <1saganeanmarius@gmail.com>
2024-10-12 17:29:11 +0300

    TEST DATA 01

--- src/main/resources/db/migration/V2024_10_04_12__INSERT_TEST_DATA.sql
+++ src/main/resources/db/migration/V2024_10_04_12__INSERT_TEST_DATA.sql
@@ -1,52 +1,52 @@
 INSERT INTO vagon_class
-    VALUES
-        ('1st class with seat', 54, 0, 1.7),
-        ('1st class without seat', 54, 25, 1.3),
-        ('2st class with seat', 72, 0, 1.5),
-        ('2st class without seat', 72, 25, 1.1);
+VALUES
+    ('1st class with seat', 54, 0, 1.7),
+    ('1st class without seat', 54, 25, 1.3),
+    ('2st class with seat', 72, 0, 1.5),
+    ('2st class without seat', 72, 25, 1.1);
 
 INSERT INTO train
-    VALUES
-        (1,'MD-NA-001'),
-        (2,'MD-NA-002'),
-        (3,'MD-NA-003'),
-        (4,'MD-NA-004');
+VALUES
+    (1,'MD-NA-001'),
+    (2,'MD-NA-002'),
+    (3,'MD-NA-003'),
+    (4,'MD-NA-004');
 
 INSERT INTO trainloadedvagons
-    VALUES
-        (1, 1),
-        (2, 2),
-        (3, 3),
-        (4, 4);
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4);
 
 INSERT INTO vagon
-    VALUES
-        (1,'1st class with seat'),
-        (2,'2st class with seat'),
-        (3,'2st class without seat'),
-        (4,'2st class without seat');
+VALUES
+    (1,'1st class with seat'),
+    (2,'2st class with seat'),
+    (3,'2st class without seat'),
+    (4,'2st class without seat');
 
 INSERT INTO trainvagon
-    VALUES
-        (1,1,1),
-        (1,2,2),
-        (1,3,3),
-        (1,4,4),
+VALUES
+    (1,1,1),
+    (1,2,2),
+    (1,3,3),
+    (1,4,4),
 
-        (2,1,1),
-        (2,2,2),
-        (2,3,3),
-        (2,4,4),
+    (2,1,1),
+    (2,2,2),
+    (2,3,3),
+    (2,4,4),
 
-        (3,1,1),
-        (3,2,2),
-        (3,3,3),
-        (3,4,4),
+    (3,1,1),
+    (3,2,2),
+    (3,3,3),
+    (3,4,4),
 
-        (4,1,1),
-        (4,2,2),
-        (4,3,3),
-        (4,4,4);
+    (4,1,1),
+    (4,2,2),
+    (4,3,3),
+    (4,4,4);
 
 
 
@@ -114,5 +114,4 @@ VALUES
     (38, 4, 4, '2025-01-02 17:15:00'),
     (39, 4, 4, '2025-01-02 17:30:00'),
     (40, 4, 4, '2025-01-02 17:45:00');
-
 
